8189aac7493902d0e9d1985a772526ac1375eae8
Jonathan Morales <jonathan.morales26756@gmail.com>
2017-08-29 15:31:23 -0400

    build is available on docker hub. files no longer neccesary for building. change kamailio image reference to point to docker hub name for saycel/kamailio:latest

--- kamailio-setup/run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sed -i "s/rhizortc.specialstories.org/$DOMAIN/g" /etc/kamailio/*
-sed -i "s/138.68.99.252/$DOMAIN_IP/g" /etc/kamailio/*
-sed -i "s/localhost\/kamailio/kamailio_mysql\/kamailio/g" /etc/kamailio/*
-service kamailio start
-tail -f /dev/null
