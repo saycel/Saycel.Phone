8886607622357b6bf50202300d6d516dbd305344
Jonathan Morales <jonathan.morales26756@gmail.com>
2017-09-15 18:36:59 -0400

    move run.sh file from docker hub to github for better control of the final images to be run

---
+++ rtpengine/run.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+rtpengine_ip=$(ping -c 1 rtpengine)
+rtpengine_ip_1=$(awk -F')' '{print $1}' <<< $rtpengine_ip)
+rtpengine_ip_2=$(awk -F'(' '{print $2}' <<< $rtpengine_ip_1)
+sed -i "s/138.68.99.252/$rtpengine_ip_2/g" /etc/rtpengine/rtpengine.conf
+sed -i "s/127.0.0.1:2223/2223/g" /etc/rtpengine/rtpengine.conf
+service ngcp-rtpengine-daemon start
+tail -f /dev/null
