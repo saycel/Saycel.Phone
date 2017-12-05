4ecfb537a92078040bc012ceb072d054399885ad
Jonathan Morales <jonathan.morales26756@gmail.com>
2017-12-05 17:04:50 -0500

    fix permissions.sh and point to mysql container

--- kamailio/config-files/permissions.sh
+++ kamailio/config-files/permissions.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-mysql -u root -p$MYSQL_ROOT_PASSWORD -h kamailio_mysql << EOF
-update mysql.user set host='%' where host='kamailio_mysql.saycelphone_default' and user='kamailio';
+mysql -u root -p$MYSQL_ROOT_PASSWORD -h mysql << EOF
+update mysql.user set host='%' where host='mysql' and user='kamailio';
 flush privileges;
 grant all on kamailio.* to 'kamailio'@'%';
-update mysql.user set host='%' where host='kamailio_mysql.saycelphone_default' and user='kamailioro';
+update mysql.user set host='%' where host='mysql' and user='kamailioro';
 flush privileges;
 grant usage on kamailio.* to 'kamailioro'@'%';
 grant select on kamailio.* to 'kamailioro'@'%';
