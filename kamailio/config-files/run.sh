efe895c5a75663a113f51075b5d2979e9a30f021
Jonathan Morales <jonathan.morales26756@gmail.com>
2017-12-05 14:37:33 -0500

    modify dockerfiles and organize files

---
+++ kamailio/config-files/run.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# replace setting in configuration files with user defined variables, necessary configurations.
+sed -i "s/rhizortc.specialstories.org/$DOMAIN/g" /etc/kamailio/*
+sed -i "s/kamailio_mysql/kamailio_mysql.saycelphone_default/g" /etc/kamailio/*
+sed -i "s/localhost\/kamailio/kamailio_mysql.saycelphone_default\/kamailio/g" /etc/kamailio/*
+# find ping for rtpengine and insert into config files.
+rtpengine_ip=$(ping -c 1 rtpengine)
+rtpengine_ip_1=$(awk -F')' '{print $1}' <<< $rtpengine_ip)
+rtpengine_ip_2=$(awk -F'(' '{print $2}' <<< $rtpengine_ip_1)
+sed -i "s/127.0.0.1/$rtpengine_ip_2/g" /etc/kamailio/*
+while ! mysqladmin ping -h "kamailio_mysql" --silent; do
+    sleep 1
+done
+kamdbctl create <<< $MYSQL_ROOT_PASSWORD & wait
+./permissions.sh & wait
+kamctl add webrtc verysecret & wait
+service kamailio start
+tail -f /dev/null
