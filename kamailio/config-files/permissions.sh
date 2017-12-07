71a5f84b55a4e70be60a8142856a986d91893240
Jonathan Morales <jonathan.morales26756@gmail.com>
2017-12-07 15:32:03 -0500

    move kamctlrc variables to .env file. edit permissions.sh to use .env variables to fix permissions fo ruse in docker-compose project

--- kamailio/config-files/permissions.sh
+++ kamailio/config-files/permissions.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 mysql -u root -p$MYSQL_ROOT_PASSWORD -h mysql << EOF
-update mysql.user set host='%' where host='mysql' and user='kamailio';
+update mysql.user set host='%' where host='mysql' and user="$DBRWUSER";
 flush privileges;
-grant all on kamailio.* to 'kamailio'@'%';
-update mysql.user set host='%' where host='mysql' and user='kamailioro';
+grant all on $DBNAME.* to "$DBRWUSER"@'%';
+update mysql.user set host='%' where host='mysql' and user="$DBROUSER";
 flush privileges;
-grant usage on kamailio.* to 'kamailioro'@'%';
-grant select on kamailio.* to 'kamailioro'@'%';
+grant usage on $DBNAME.* to "$DBROUSER"@'%';
+grant select on $DBNAME.* to "$DBROUSER"@'%';
 flush privileges;
 EOF
